d057070ac77eb017bc95fd564c9b083199e3d627
k-omura <26690530+k-omura@users.noreply.github.com>
2018-08-29 21:31:48 +0900

    Update SSD1331_ttf.h

--- src/SSD1331_ttf.h
+++ src/SSD1331_ttf.h
@@ -1,6 +1,10 @@
 #if !defined TRUETYPE_H
-#include "truetype.h"
+#include "truetype_Arduino.h"
 #endif /* TRUETYPE_H */
+
+#if !defined SPI_FULLCOLOR_H
+#include "spi_fullcolor_basis.h"
+#endif /* SPI_FULLCOLOR_H */
 
 //OLED command list
 //#define PORTRAIT
@@ -30,40 +34,11 @@
 #endif
 //OLED command list end
 
-typedef union {
-  uint16_t raw;
-  struct {
-    unsigned int r : 5;
-    unsigned int g : 6;
-    unsigned int b : 5;
-  } rgb;
-} rgb16_t;
-
-class outputttf {
+class ssd1331_ttf : public outputttf {
   public:
-    outputttf(SPIClass *_spi);
+    ssd1331_ttf(SPIClass *_spi);
 
-    void setTruetype(truetypeClass *_ttf);
-    void setSPIpin(uint8_t _cs, uint8_t _reset, uint8_t _dc);
-    void setColor(uint16_t _inside, uint16_t _outline, uint16_t _background);
-
-    uint8_t displayString(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t characterSpace);
-    uint8_t displayString(uint8_t start_x, uint8_t start_y, const wchar_t character[], uint8_t characterSize, uint8_t characterSpace);
-    uint8_t displayMonospaced(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t monospacedWidth);
-
-  private:
-    static SPIClass *spi;
-    truetypeClass *font;
-
-    uint8_t TFT_CS;
-    uint8_t TFT_RESET;
-    uint8_t TFT_DC;
-
-    uint16_t outlineColor;
-    uint16_t insideColor;
-    uint16_t backgroundColor;
-
-    uint8_t outputTFT(uint8_t _x, uint8_t y_, uint8_t _height, uint8_t monospacedWidth = 0);
-    void SSD1331_set_rect(uint8_t _x1, uint8_t _x2, uint8_t _y1, uint8_t _y2);
-    void SSD1331_fill_rect(uint8_t _x1, uint8_t _x2, uint8_t _y1, uint8_t _y2, uint16_t _outline, uint16_t _innner);
+  protected:
+    virtual void set_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2) override;
+    virtual void fill_rect(uint8_t _x1, uint8_t _x2, uint8_t _y1, uint8_t _y2, uint16_t _color) override;
 };
