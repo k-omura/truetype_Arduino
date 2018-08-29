39d5a926643b0f7bca0d3b531cf5500e5bb5690e
k-omura <26690530+k-omura@users.noreply.github.com>
2018-08-29 22:20:58 +0900

    Update spi_fullcolor_basis.cpp

--- src/spi_fullcolor_basis.cpp
+++ src/spi_fullcolor_basis.cpp
@@ -1,29 +1,29 @@
 #include "spi_fullcolor_basis.h"
 
-SPIClass *outputttf::spi;
+SPIClass *ttfSpiFullColor::spi;
 
 //constructor
-outputttf::outputttf(SPIClass *_spi) {
+ttfSpiFullColor::ttfSpiFullColor(SPIClass *_spi) {
   this->spi = _spi;
 }
 
-void outputttf::setTruetype(truetypeClass *_ttf) {
+void ttfSpiFullColor::setTruetype(truetypeClass *_ttf) {
   this->font = _ttf;
 }
 
-void outputttf::setSPIpin(uint8_t _cs, uint8_t _reset, uint8_t _dc) {
+void ttfSpiFullColor::setSPIpin(uint8_t _cs, uint8_t _reset, uint8_t _dc) {
   this->TFT_CS = _cs;
   this->TFT_RESET = _reset;
   this->TFT_DC = _dc;
 }
 
-void outputttf::setColor(uint16_t _inside, uint16_t _outline, uint16_t _background) {
+void ttfSpiFullColor::setColor(uint16_t _inside, uint16_t _outline, uint16_t _background) {
   this->insideColor = _inside;
   this->outlineColor = _outline;
   this->backgroundColor = _background;
 }
 
-uint8_t outputttf::displayString(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t characterSpace) {
+uint8_t ttfSpiFullColor::displayString(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t characterSpace) {
   uint8_t c = 0;
   
   while (character[c]) {
@@ -37,7 +37,7 @@ uint8_t outputttf::displayString(uint8_t start_x, uint8_t start_y, const char ch
   return start_x;
 }
 
-uint8_t outputttf::displayString(uint8_t start_x, uint8_t start_y, const wchar_t character[], uint8_t characterSize, uint8_t characterSpace) {
+uint8_t ttfSpiFullColor::displayString(uint8_t start_x, uint8_t start_y, const wchar_t character[], uint8_t characterSize, uint8_t characterSpace) {
   uint8_t c = 0;
   
   while (character[c]) {
@@ -51,7 +51,7 @@ uint8_t outputttf::displayString(uint8_t start_x, uint8_t start_y, const wchar_t
   return start_x;
 }
 
-uint8_t outputttf::displayMonospaced(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t monospacedWidth) {
+uint8_t ttfSpiFullColor::displayMonospaced(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t monospacedWidth) {
   uint8_t c = 0;
   
   while (character[c]) {
@@ -65,7 +65,7 @@ uint8_t outputttf::displayMonospaced(uint8_t start_x, uint8_t start_y, const cha
   return start_x;
 }
 
-uint8_t outputttf::outputTFT(uint8_t _x, uint8_t _y, uint8_t _height, uint8_t monospacedWidth) {
+uint8_t ttfSpiFullColor::outputTFT(uint8_t _x, uint8_t _y, uint8_t _height, uint8_t monospacedWidth) {
   //---Code for displaying a bitmap
   uint8_t width = font->generateBitmap(_height);
 
@@ -106,7 +106,7 @@ uint8_t outputttf::outputTFT(uint8_t _x, uint8_t _y, uint8_t _height, uint8_t mo
   return width;
 }
 
-void outputttf::fill_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2, uint16_t _color) {
+void ttfSpiFullColor::fill_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2, uint16_t _color) {
   uint32_t repeat = (_x2 - _x1 + 1) * (_y2 - _y1 + 1);
   
   digitalWrite(this->TFT_CS, LOW);
