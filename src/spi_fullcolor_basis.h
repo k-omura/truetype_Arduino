78a0a11d3d86cd1ba93a412e96cc83feba1fbb7a
k-omura <26690530+k-omura@users.noreply.github.com>
2018-12-30 17:13:11 +0900

    Update spi_fullcolor_basis.h

--- src/spi_fullcolor_basis.h
+++ src/spi_fullcolor_basis.h
@@ -20,6 +20,7 @@ class ttfSpiFullColor {
     void setTruetype(truetypeClass *_ttf);
     void setSPIpin(uint8_t _cs, uint8_t _reset, uint8_t _dc);
     void setColor(uint16_t _inside, uint16_t _outline, uint16_t _background);
+    void setUnderLine(bool _allowUnderLine);
 
     uint8_t displayString(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t characterSpace);
     uint8_t displayString(uint8_t start_x, uint8_t start_y, const wchar_t character[], uint8_t characterSize, uint8_t characterSpace);
@@ -29,16 +30,17 @@ class ttfSpiFullColor {
     static SPIClass *spi;
     truetypeClass *font;
 
-    uint8_t TFT_CS;
-    uint8_t TFT_RESET;
-    uint8_t TFT_DC;
+    uint8_t Display_CS;
+    uint8_t Display_RESET;
+    uint8_t Display_DC;
 
     uint16_t outlineColor;
     uint16_t insideColor;
     uint16_t backgroundColor;
+    bool underLine = false;
 
-    uint8_t outputTFT(uint8_t _x, uint8_t y_, uint8_t _height, uint8_t monospacedWidth = 0);
+    virtual uint8_t outputTFT(uint8_t _x, uint8_t y_, uint8_t _height, uint8_t monospacedWidth = 0);
 
     virtual void set_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2) = 0;
-    virtual void fill_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2, uint16_t _color);
+    virtual void fill_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2);
 };
