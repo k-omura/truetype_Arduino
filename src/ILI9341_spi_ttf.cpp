3d253494279ecbb5db0bec3647bd6cd99ebcae9a
k-omura <26690530+k-omura@users.noreply.github.com>
2018-12-30 17:11:18 +0900

    Update ILI9341_spi_ttf.cpp

--- src/ILI9341_spi_ttf.cpp
+++ src/ILI9341_spi_ttf.cpp
@@ -5,17 +5,17 @@ ILI9341_spi_ttf::ILI9341_spi_ttf(SPIClass *_spi) : ttfSpiFullColor(_spi) {};
 
 //Rectangle setting required for drawing ILI9341
 void ILI9341_spi_ttf::set_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2) {
-  digitalWrite(this->TFT_DC, LOW);
+  digitalWrite(this->Display_DC, LOW);
   spi->transfer(CMD_COLUMN_ADDRESS_SET);
-  digitalWrite(this->TFT_DC, HIGH);
+  digitalWrite(this->Display_DC, HIGH);
   spi->transfer16(_x1);
   spi->transfer16(_x2);
-  digitalWrite(this->TFT_DC, LOW);
+  digitalWrite(this->Display_DC, LOW);
   spi->transfer(CMD_PAGE_ADDRESS_SET);
-  digitalWrite(this->TFT_DC, HIGH);
+  digitalWrite(this->Display_DC, HIGH);
   spi->transfer16(_y1);
   spi->transfer16(_y2);
-  digitalWrite(this->TFT_DC, LOW);
+  digitalWrite(this->Display_DC, LOW);
   spi->transfer(CMD_MEMORY_WRITE);
-  digitalWrite(this->TFT_DC, HIGH);
+  digitalWrite(this->Display_DC, HIGH);
 }
