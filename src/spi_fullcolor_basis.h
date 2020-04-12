8cc44e0d17688cb3522388254c5319f3d5300e2a
k-omura <26690530+k-omura@users.noreply.github.com>
2020-04-12 21:22:30 +0900

    Update spi_fullcolor_basis.h

--- src/spi_fullcolor_basis.h
+++ src/spi_fullcolor_basis.h
@@ -24,18 +24,30 @@ class ttfSpiFullColor {
   public:
     ttfSpiFullColor(SPIClass *_spi);
 
+    void optimisationInit();
+
     void setTruetype(truetypeClass *_ttf);
     void setSPIpin(uint8_t _cs, uint8_t _reset, uint8_t _dc);
     void setColor(uint16_t _inside, uint16_t _outline, uint16_t _background);
     void setUnderLine(bool _allowUnderLine);
 
-    uint8_t displayString(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t characterSpace);
-    uint8_t displayString(uint8_t start_x, uint8_t start_y, const wchar_t character[], uint8_t characterSize, uint8_t characterSpace);
-    uint8_t displayMonospaced(uint8_t start_x, uint8_t start_y, const char character[], uint8_t characterSize, uint8_t monospacedWidth);
+    uint8_t displayString(uint16_t start_x, uint16_t start_y, const char character[], uint16_t characterSize, uint8_t characterSpace);
+    uint8_t displayString(uint16_t start_x, uint16_t start_y, const wchar_t character[], uint16_t characterSize, uint8_t characterSpace);
+    uint8_t displayMonospaced(uint16_t start_x, uint16_t start_y, const char character[], uint16_t characterSize, uint8_t monospacedWidth);
 
   protected:
     static SPIClass *spi;
+
+    bool optimisation;
+#if defined(_VARIANT_ARDUINO_STM32_)
+    SPI_HandleTypeDef hspi1;
+    DMA_HandleTypeDef hdma_spi1_tx;
+#endif
+
     truetypeClass *font;
+
+    uint16_t tftWidth;
+    uint16_t tftHeifht;
 
     uint8_t Display_CS;
     uint8_t Display_RESET;
@@ -47,10 +59,10 @@ class ttfSpiFullColor {
     bool underLine = false;
 
     void spiDMAHundler();
-    void dmaSetting();
+    void optimisationSetting();
     void spiSendbuf(uint8_t *_buf, uint16_t _length);
 
-    virtual uint8_t outputTFT(uint8_t _x, uint8_t y_, uint8_t _height, uint8_t monospacedWidth = 0);
+    virtual uint8_t outputTFT(uint16_t _x, uint16_t y_, uint16_t _height, uint8_t monospacedWidth = 0);
     virtual void set_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2) = 0;
     virtual void fill_rect(uint16_t _x1, uint16_t _x2, uint16_t _y1, uint16_t _y2);
 };
